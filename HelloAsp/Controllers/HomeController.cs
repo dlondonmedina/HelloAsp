84075b1e314cae44a7800acf586fbdac22167725
Dylan Medina <medinad@uw.edu>
2019-05-17 16:06:47 -0700

    Added Model to Home Controller

--- HelloAsp/Controllers/HomeController.cs
+++ HelloAsp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelloAsp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,13 @@ namespace HelloAsp.Controllers
     {
         public ActionResult Index()
         {
+            var myData = new HomeViewModel
+            {
+                PhoneCount = 10,
+                UserCount = 15,
+                LastAccessDate = DateTime.Now
+            };
+
             return View();
         }
 
